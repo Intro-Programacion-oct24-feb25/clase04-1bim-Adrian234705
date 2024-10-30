ee3475ffcb11f32a14840a43fde2c39c9d2b3dfe
Adrian Urgiles <arurgiles2@utpl.edu.ec>
2024-10-30 11:45:15 -0500

    Comentario corregido en clase

--- Ejemplo01/src/ejemplo01/Ejemplo01.java
+++ Ejemplo01/src/ejemplo01/Ejemplo01.java
@@ -16,12 +16,13 @@ public class Ejemplo01 {
      */
     public static void main(String[] args) {
         // Ejemplo 
+        // no puedo cambiar la variable porque ya esta asignada 
         String nombreEstudiante;
         nombreEstudiante = "René Elizalde" ;
         System.out.println(nombreEstudiante);
         System.out.println("René Elizalde");
-        nombreEstudiante = "Rene Elizalde" ; 
+        nombreEstudiante ="Rene Elizalde" ; 
         System.out.println(nombreEstudiante);
     }
     
-}
+} 
