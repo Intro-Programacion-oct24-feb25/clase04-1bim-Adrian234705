d4d25a96f3f62e542d4c74c7a51ae4cec1c26172
Adrian Urgiles <arurgiles2@utpl.edu.ec>
2024-10-30 11:30:32 -0500

    Comentario corregido en clase

--- Ejemplo01/src/ejemplo01/Ejemplo01.java
+++ Ejemplo01/src/ejemplo01/Ejemplo01.java
@@ -17,10 +17,10 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         // Ejemplo 
         String nombreEstudiante;
-        nombreEstudiante = "René Elizalde";
+        nombreEstudiante = "René Elizalde" ;
         System.out.println(nombreEstudiante);
         System.out.println("René Elizalde");
-        nombreEstudiante = "Felipe Elizalde";
+        nombreEstudiante = "Rene Elizalde" ; 
         System.out.println(nombreEstudiante);
     }
     
